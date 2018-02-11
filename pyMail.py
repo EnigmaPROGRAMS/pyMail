ca046789df1474b831d0a7658ddb36edf5b8ff64
james <enigma@memeware.net>
2018-02-11 15:41:50 +0000

    Update pyMail.py

--- pyMail.py
+++ pyMail.py
@@ -11,81 +11,85 @@ from bs4 import BeautifulSoup
 from sys import platform
 from os import system
 
-def clear_screen():
-    if platform == "linux" or platform == "linux2" or platform == "darwin":
-        system('clear')
-    elif platform == "win32":
-        system('cls')
-    
-    print "               ,--.   ,--.        ,--.,--."
-    print " ,---.,--. ,--.|   `.'   | ,--,--.`--'|  |"
-    print "| .-. |\  '  / |  |'.'|  |' ,-.  |,--.|  | \x1B[3mcreated by james\x1B[23m"
-    print "| '-' ' \   '  |  |   |  |\ '-'  ||  ||  |"
-    print "|  |-'.-'  /   `--'   `--' `--`--'`--'`--'"
-    print "`--'  `---'                               "
-    print "\n"
 
-def leaving_message():
-    clear_screen()
-    print "thanks for using pyMail!\n"
-    quit()
-
-# lets gain some user info first
-clear_screen()
-local_email = raw_input("type in your google email: ")
-local_password = getpass.getpass(prompt="type in your google password: ")
-print "\n\n"
-
-# gather the site data
 try:
+    def clear_screen():
+        if platform == "linux" or platform == "linux2" or platform == "darwin":
+            system('clear')
+        elif platform == "win32":
+            system('cls')
+        
+        print "               ,--.   ,--.        ,--.,--."
+        print " ,---.,--. ,--.|   `.'   | ,--,--.`--'|  |"
+        print "| .-. |\  '  / |  |'.'|  |' ,-.  |,--.|  | \x1B[3mcreated by james\x1B[23m"
+        print "| '-' ' \   '  |  |   |  |\ '-'  ||  ||  |"
+        print "|  |-'.-'  /   `--'   `--' `--`--'`--'`--'"
+        print "`--'  `---'                               "
+        print "\n"
+
+    def leaving_message():
+        clear_screen()
+        print "thanks for using pyMail!\n"
+        quit()
+
+    # lets gain some user info first
     clear_screen()
-    site = BeautifulSoup(urllib2.urlopen(raw_input("enter a sitename to scrape: ")), "html.parser").prettify()
+    local_email = raw_input("type in your google email: ")
+    local_password = getpass.getpass(prompt="type in your google password: ")
     print "\n\n"
-except:
-    clear_screen()
-    print "pyMail couldn't succeed in scraping the site - it may have scrape security"
-    time.sleep(2)
-    leaving_message()
 
-# parsing the site data to find the emails
-emails = list(set(re.findall('[\w\.-]+@[\w\.-]+', site))) # gathered emails
-if not emails:
-    clear_screen()
-    print "pyMail didn't find any emails :("
-    time.sleep(2)
-    leaving_message()
-else:
-    clear_screen()
-    print 'pyMail found this: ' + ' | '.join(emails) + '\n\n'
-    send_email_question = raw_input("would you like to send a mass email? [Y|y] ")
-    emails = ', '.join(emails)
-
-def email(emails):
-    clear_screen()
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    
+    # gather the site data
     try:
-        server.login(local_email, local_password)
+        clear_screen()
+        site = BeautifulSoup(urllib2.urlopen(raw_input("enter a sitename to scrape: ")), "html.parser").prettify()
+        print "\n\n"
     except:
-        print "email authentication has failed - did you use the right login details?"
+        clear_screen()
+        print "pyMail couldn't succeed in scraping the site - it may have scrape security"
         time.sleep(2)
         leaving_message()
 
-    msg = MIMEMultipart()
-    msg['From'] = local_email
-    msg['To'] = emails
-    msg['Subject'] = raw_input("type a subject: ")
-    body = raw_input("type a body: ")
-    msg.attach(MIMEText(body, 'plain'))
-    text = msg.as_string()
-    server.sendmail(local_email, emails, text)
-    print "\n\nthe mail has been sent"
-    time.sleep(2)
-    server.quit()
-    leaving_message()
+    # parsing the site data to find the emails
+    emails = list(set(re.findall('[\w\.-]+@[\w\.-]+\.\w+', site))) # gathered emails
+    if not emails:
+        clear_screen()
+        print "pyMail didn't find any emails :("
+        time.sleep(2)
+        leaving_message()
+    else:
+        clear_screen()
+        print 'pyMail found this: ' + ' | '.join(emails) + '\n\n'
+        send_email_question = raw_input("would you like to send a mass email? [Y|y] ")
+        emails = ', '.join(emails)
 
-if send_email_question == "Y" or send_email_question == "y":
-    email(emails)
-else:
+    def email(emails):
+        clear_screen()
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        
+        try:
+            server.login(local_email, local_password)
+        except:
+            print "email authentication has failed - did you use the right login details?"
+            time.sleep(2)
+            leaving_message()
+
+        msg = MIMEMultipart()
+        msg['From'] = local_email
+        msg['To'] = emails
+        msg['Subject'] = raw_input("type a subject: ")
+        body = raw_input("type a body: ")
+        msg.attach(MIMEText(body, 'plain'))
+        text = msg.as_string()
+        server.sendmail(local_email, emails, text)
+        print "\n\nthe mail has been sent"
+        time.sleep(2)
+        server.quit()
+        leaving_message()
+
+    if send_email_question == "Y" or send_email_question == "y":
+        email(emails)
+    else:
+        leaving_message()
+except KeyboardInterrupt:
     leaving_message()
